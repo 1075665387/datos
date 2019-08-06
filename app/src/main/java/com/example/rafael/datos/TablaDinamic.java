f9da66d356073fb00f85aa32d99477592dfe7752
rafael <elrafafa1992@hotmail.com>
2019-08-06 09:38:16 -0500

    Initial commit

---
+++ app/src/main/java/com/example/rafael/datos/TablaDinamic.java
@@ -0,0 +1,4 @@
+package com.example.rafael.datos;
+
+public class TablaDinamic {
+}
---
+++ app/src/main/java/com/example/rafael/datos/galeriaActivity.java
@@ -0,0 +1,13 @@
+package com.example.rafael.datos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class galeriaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_galeria);
+    }
+}
